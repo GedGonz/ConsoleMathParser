3038c10fccf0b37dbc006f4e8e181abbad9a1bfc
Gerald Gonzalez <gedgonz7@gmail.com>
2021-05-18 16:50:54 -0600

    identifying each line of code

--- ConsoleMathParser/Program.cs
+++ ConsoleMathParser/Program.cs
@@ -7,20 +7,28 @@ namespace ConsoleMathParser
     {
         static void Main(string[] args)
         {
-            //Expression basic
-            Expression e = new Expression("2+(3-5)^2");
-            var result=e.calculate();
-            //Console.WriteLine("Resultado: "+ result);
+            /***Expression basic***/
+           //expression 
+           Expression e = new Expression("2+(3-5)^2");
+           //perform the calculation
+            var result =e.calculate();
+           //Console.WriteLine("Resultado: "+ result);
 
+           /***Triangle area***/
 
-
-            //Triangle area
+            //declaration function 
             Function At = new Function("At(b,h) = 1/2 * b * h");
-            Expression e1 = new Expression("At(2,4)", At);
+            // send value in expression 
+            Expression e1 = new Expression("At(2,4)", At); 
+
             Console.WriteLine("Expression: " + e1.getExpressionString() + "" + e1.calculate());
+
+            //create argument with value
             Argument arg1 = new Argument("b=3");
             Argument arg2 = new Argument("h=10");
-            Expression e2 = new Expression("At(b,h)", At, arg1, arg2);
+            //send argument to expression
+            Expression e2 = new Expression("At(b,h)", At, arg1, arg2); 
+
             Console.WriteLine("Expression: " + e2.getExpressionString() + "" + e2.calculate());
         }
     }
