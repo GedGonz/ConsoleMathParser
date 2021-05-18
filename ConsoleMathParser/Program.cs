9995bcc4d40a679bfe204fc69b0edcef6050762e
Gerald Gonzalez <gedgonz7@gmail.com>
2021-05-18 16:46:18 -0600

    triangle area calculation

--- ConsoleMathParser/Program.cs
+++ ConsoleMathParser/Program.cs
@@ -10,8 +10,18 @@ namespace ConsoleMathParser
             //Expression basic
             Expression e = new Expression("2+(3-5)^2");
             var result=e.calculate();
-            Console.WriteLine("Resultado: "+ result);
+            //Console.WriteLine("Resultado: "+ result);
 
+
+
+            //Triangle area
+            Function At = new Function("At(b,h) = 1/2 * b * h");
+            Expression e1 = new Expression("At(2,4)", At);
+            Console.WriteLine("Expression: " + e1.getExpressionString() + "" + e1.calculate());
+            Argument arg1 = new Argument("b=3");
+            Argument arg2 = new Argument("h=10");
+            Expression e2 = new Expression("At(b,h)", At, arg1, arg2);
+            Console.WriteLine("Expression: " + e2.getExpressionString() + "" + e2.calculate());
         }
     }
 }
